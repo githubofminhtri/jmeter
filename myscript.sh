7af980871cf9a3d88f34c2715abf519a82eb303a
chishiki <githubofminhtri@hotmail.com>
2023-10-22 21:41:04 +0700

    add new script

---
+++ myscript.sh
@@ -0,0 +1,8 @@
+echo "hello"
+mkdir Pay-Function
+cd Pay-Function
+mkdir Pay-Cash-Card Pay-Sand-Dollar-Card Pay-Credit-Card Pay-Gift-Card
+mkdir Pay-Cash-Card/dataInput Pay-Cash-Card/dataOutput
+mkdir Pay-Sand-Dollar-Card/dataInput Pay-Sand-Dollar-Card/dataOutput
+mkdir Pay-Credit-Card/dataInput Pay-Credit-Card/dataOutput
+mkdir Pay-Gift-Card/dataInput Pay-Gift-Card/dataOutput
